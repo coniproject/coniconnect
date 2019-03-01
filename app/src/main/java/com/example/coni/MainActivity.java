b41a61ee32b046f32c34c2ab0293c088c629ee8d
Aya <ayabeltran14@gmail.com>
2019-03-01 12:52:05 +0800

    updates;#

--- app/src/main/java/com/example/coni/MainActivity.java
+++ app/src/main/java/com/example/coni/MainActivity.java
@@ -1,23 +1,23 @@
 package com.example.coni;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnlogin, btnsignup;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.conilogo);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
-
 
 
         btnlogin = findViewById(R.id.btn_login);
